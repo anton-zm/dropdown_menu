d3bf70d54a4d4a74fcbe78f1d28008e47351273b
anton-zm <antonzm34@gmail.com>
2023-07-31 21:23:50 +0300

    chore: set environment

--- next.config.js
+++ next.config.js
@@ -1,28 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    webpack(config) {
-        config.module.rules.push({
-            test: /\.svg$/i,
-            issuer: /\.[jt]sx?$/,
-            loader: '@svgr/webpack',
-            options: {
-                svgo: true,
-                svgoConfig: {
-                    plugins: [
-                        {
-                            name: 'preset-default',
-                            params: {
-                                overrides: { removeViewBox: false }
-                            }
-                        }
-                    ]
-                }
-            }
-        });
+  reactStrictMode: true,
+}
 
-        return config;
-    }
-};
-
-module.exports = nextConfig;
+module.exports = nextConfig
