8715462f4b63299c95c2978293d87bd919df1d1a
anton-zm <antonzm34@gmail.com>
2023-08-01 00:23:01 +0300

    feat: layout component

--- components/dropdown-menu.tsx
+++ components/dropdown-menu.tsx
@@ -1,10 +1,47 @@
-import { DropdownMenuProps } from '@/types';
+import { DropdownMenuProps, Location } from '@/types';
 import { observer } from 'mobx-react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 export const DropdownMenu = observer(
     ({ options, trigger }: DropdownMenuProps) => {
-        return <Wrapper></Wrapper>;
+        const [opened, setOpened] = useState(false);
+        const [location, setLocation] = useState<Location>('top-right');
+
+        useEffect(() => {
+            //TODO
+            setLocation('top-left');
+        }, []);
+
+        return (
+            <Trigger
+                className="clickable relative"
+                onClick={() => setOpened(!opened)}
+            >
+                <p>{trigger}</p>
+                {opened && (
+                    <Menu location={location}>
+                        {options.map((e, i) => (
+                            <div key={i} className="flex gap10">
+                                <p onClick={e.onClick} className="">
+                                    {e.text}
+                                </p>
+                                {e.icon}
+                            </div>
+                        ))}
+                    </Menu>
+                )}
+            </Trigger>
+        );
     }
 );
-const Wrapper = styled.div``;
+const Trigger = styled.div``;
+
+const Menu = styled.div<{ location: Location }>`
+    position: absolute;
+    top: calc(100% + 5px);
+    left: 0;
+    width: 260px;
+    border-radius: 4px;
+    box-shadow: 0px 6px 16px 0px rgba(0, 0, 0, 0.2);
+`;
--- types.ts
+++ types.ts
@@ -1,5 +1,11 @@
 import { ReactNode } from 'react';
 
+export type Location =
+    | 'top-left'
+    | 'top-right'
+    | 'bottom-left'
+    | 'bottom-right';
+
 export interface IDropdownOption {
     text: string;
     icon?: ReactNode;
